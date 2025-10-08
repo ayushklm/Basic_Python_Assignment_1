48b3f36265058f5470f97f124961e5d59cef014f
ayushklm <ayushdho123@gmail.com>
2025-10-08 22:12:39 +0530

    Add files via upload
    “Added Task 1 and Task 2 Python scripts”

---
+++ task1.py.py
@@ -0,0 +1,17 @@
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second number :"))
+#Addition
+c = a+b
+print("Addition:",c)
+#Substraction
+d = a-b
+print("Substraction:",d)
+#Multiplication
+e = a*b
+print("Multiplication:",e)
+#Division
+f = a/b
+print("Division:",f)
+
+
+
